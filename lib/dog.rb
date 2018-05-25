5925d4790d66cf8c211bce2d37daf88747298646
Anusha Konatala <anu.konatala@gmail.com>
2018-05-25 19:33:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,22 @@
 # Your code goes here!
 class Dog
+  def initilize(name)
+    @name = name
+  end
   
+  def name=(name)
+    @name
+  end
+  
+  def name
+    @name
+  end
+  
+  def bark=(bark)
+    @bark = bark
+  end
+  
+  def bark
+    @bark
+  end
 end
